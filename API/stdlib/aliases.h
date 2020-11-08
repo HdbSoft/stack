8b242135eabbc604674ba8211d6f7a37c9f601c1
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-11-08 13:56:33 +0000

    Update print aliases

--- API/stdlib/aliases.h
+++ API/stdlib/aliases.h
@@ -2,7 +2,7 @@
 #define STARLANG_ALIASES_H
 
 #define main\
-	i32 main(int argc, String* argv)
+	int main(int argc, String* argv)
 
 #define loop\
 	while(1)
@@ -11,16 +11,16 @@
 #define jmp goto
 #define out return
 
-#define StrLog(s)      printf("%s",   s)
-#define StrLogln(s)    printf("%s\n", s)
+#define WriteString(s)   printf("%s",   s)
+#define WriteStringln(s) printf("%s\n", s)
 
-#define IntLog(i)      printf("%d",   i)
-#define IntLogln(i)    printf("%d\n", i)
+#define WriteInt(i)      printf("%d",   i)
+#define WriteIntln(i)    printf("%d\n", i)
 
-#define FloatLog(f)    printf("%f",   f)
-#define FloatLogln(f)  printf("%f\n", f)
+#define WriteFloat(f)    printf("%f",   f)
+#define WriteFloatln(f)  printf("%f\n", f)
 
-#define CharLog(c)     printf("%c",   c)
-#define CharLogln(c)   printf("%c\n", c)
+#define WriteChar(c)     printf("%c",   c)
+#define WriteCharln(c)   printf("%c\n", c)
 
 #endif //Starlang aliases.h
