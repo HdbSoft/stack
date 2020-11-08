faef23d508e858cea0f861e2d3b6821da515f192
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-11-08 14:57:36 +0000

    Create Starlang compiler

---
+++ cli/src/main.rs
@@ -0,0 +1,31 @@
+use std::process::Command;
+use structopt::StructOpt;
+use exitfailure::ExitFailure;
+
+mod starlang;
+
+#[derive(StructOpt)]
+struct Cli {
+	#[structopt(parse(from_os_str))]
+	source: std::path::PathBuf,
+}
+
+fn main() -> Result<(), ExitFailure> {
+	let args     = Cli::from_args();
+	let compiler = starlang::Starlang::new();
+
+	let source = match std::fs::read_to_string(args.source) {
+		Err(error) => return Err(error.into()),
+		Ok(source) => source,
+	};
+
+	compiler.cmp(source, "output.c");
+
+	let gcc = Command::new("gcc")
+	                  .arg("-w")
+					  .arg("output.c")
+					  .output()
+					  .expect("Could not compile C code");
+
+	Ok(())
+}
